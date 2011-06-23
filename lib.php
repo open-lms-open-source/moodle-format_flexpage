dc4253e9a90a5435e76f375cbf3b2248295347f4
Mark Nielsen <mark@moodlerooms.com>
2011-06-23 22:01:00 +0000

    INT-1406: Adding flexpage link type
    git-svn-id: http://svn.moodlerooms.com/Moodle/2.0/branches/plugins@15582 f529b7b8-409e-e885-f664-9768b4ed42ab

--- lib.php
+++ lib.php
@@ -46,8 +46,7 @@ function callback_flexpage_load_content(global_navigation &$navigation, stdClass
         if ($parentnode->hidden) {
             continue;
         }
-        $url  = new moodle_url('/course/view.php', array('id' => $course->id, 'pageid' => $page->get_id()));
-        $node = $parentnode->add(format_string($page->get_display_name()), $url, navigation_node::TYPE_CUSTOM, null, $page->get_id());
+        $node = $parentnode->add(format_string($page->get_display_name()), $page->get_url(), navigation_node::TYPE_CUSTOM, null, $page->get_id());
         $node->hidden = (!$cache->is_page_available($page));
         $parentnodes[$page->get_id()] = $node;
 
