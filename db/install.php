1a24d73293fa1fd9b40c761580932b48ab8c27a3
Mark Nielsen <mark@moodlerooms.com>
2011-06-27 20:15:16 +0000

    INT-1406: Full 1.9 to 2.0 migration path completed, testing is still needed
    git-svn-id: http://svn.moodlerooms.com/Moodle/2.0/branches/plugins@15607 f529b7b8-409e-e885-f664-9768b4ed42ab

--- db/install.php
+++ db/install.php
@@ -9,10 +9,113 @@ require_once($CFG->dirroot.'/course/format/flexpage/repository/page.php');
  */
 require_once($CFG->dirroot.'/course/format/flexpage/repository/condition.php');
 
+/**
+ * @see block_flexpagenav_repository_menu
+ */
+require_once($CFG->dirroot.'/blocks/flexpagenav/repository/menu.php');
+
+/**
+ * @see block_flexpagenav_repository_link
+ */
+require_once($CFG->dirroot.'/blocks/flexpagenav/repository/link.php');
+
 function xmldb_format_flexpage_install() {
     global $DB, $CFG;
 
-    // @todo Migration of navigation
+    // Tracks format_page.id to format_flexpage_page.id
+    $pageidmap = array();
+    // Tracks pagemenu.id to block_flexpagenav_menu.id
+    $menuidmap = array();
+
+/// Migration of mod/pagemenu to blocks/flexpagenav
+    if ($DB->get_manager()->table_exists('pagemenu')) {
+        // Link config names got renamed...
+        $configrename = array(
+            'linkname' => 'label', 'linkurl' => 'url', 'moduleid' => 'cmid', 'pageid' => 'pageid', 'ticketname' => 'label', 'ticketsubject' => 'subject',
+        );
+        $linktyperename = array(
+            'link' => 'url', 'module' => 'mod', 'page' => 'flexpage', 'ticket' => 'ticket',
+        );
+        $renderrename = array(
+            'list' => 'tree', 'select' => 'select',
+        );
+
+        $menurepo = new block_flexpagenav_repository_menu();
+        $linkrepo = new block_flexpagenav_repository_link();
+
+        $records  = $DB->get_recordset('pagemenu');
+        foreach ($records as $record) {
+            $menu = new block_flexpagenav_model_menu();
+            $menu->set_couseid($record->course)
+                 ->set_name($record->name)
+                 ->set_render($renderrename[$record->render])
+                 ->set_displayname($record->displayname)
+                 ->set_useastab(0);
+            $menurepo->save_menu($menu);
+
+            // Save old to new
+            $menuidmap[$record->id] = $menu->get_id();
+
+            if ($linkrecords = $DB->get_records('pagemenu_links', array('pagemenuid' => $record->id), 'previd ASC')) {
+                $datarecords = $DB->get_records_sql('
+                    SELECT d.*
+                      FROM {pagemenu_links} l
+                INNER JOIN {pagemenu_link_data} d ON l.id = d.linkid
+                     WHERE l.pagemenuid = ?
+                ', array($record->id));
+
+                $linkrecord = reset($linkrecords);
+                $linkid = $linkrecord->id;
+
+                $weight = 0;
+                while ($linkid) {
+                    if (!array_key_exists($linkid, $linkrecords)) {
+                        continue;
+                    }
+                    $linkrecord = $linkrecords[$linkid];
+
+                    $data = array();
+                    foreach ($datarecords as $datarecord) {
+                        if ($datarecord->linkid == $linkrecord->id) {
+                            $name = $configrename[$datarecord->name];
+
+                            if (array_key_exists($name, $data)) {
+                                $value = $data[$name];
+                                if (!is_array($value)) {
+                                    $value = array($value);
+                                }
+                                $value[] = $datarecord->value;
+                                $data[$name] = $value;
+                            } else {
+                                $data[$name] = $datarecord->value;
+                            }
+                        }
+                    }
+                    $link = new block_flexpagenav_model_link();
+                    $link->set_menuid($menu->get_id())
+                         ->set_type($linktyperename[$linkrecord->type])
+                         ->set_weight($weight);
+                    $linkrepo->save_link($link);
+
+                    $configs = array();
+                    foreach ($data as $name => $value) {
+                        if (is_array($value)) {
+                            $value = implode(',', $value);
+                        }
+                        $configs[] = new block_flexpagenav_model_link_config($name, $value);
+                    }
+                    $linkrepo->save_link_config($link, $configs);
+
+                    $linkid = $linkrecord->nextid;
+                    $weight++;
+                }
+            }
+
+        }
+        $records->close();
+    }
+
+/// Migration of course/format/page to course/format/flexpage
 
     // Handle plugin name changes...
     $DB->set_field('course', 'format', 'flexpage', array('format' => 'page'));
@@ -23,7 +126,6 @@ function xmldb_format_flexpage_install() {
         set_config('theme', 'flexpage');
     }
     if ($DB->get_manager()->table_exists('format_page')) {
-        $oldtonew = array();
         $pagerepo = new course_format_flexpage_repository_page();
         $condrepo = new course_format_flexpage_repository_condition();
         $records  = $DB->get_recordset('format_page', null, 'courseid, parent, sortorder');
@@ -38,11 +140,11 @@ function xmldb_format_flexpage_install() {
             }
             // Re-map parent
             if (!empty($record->parent)) {
-                if (!array_key_exists($record->parent, $oldtonew)) {
+                if (!array_key_exists($record->parent, $pageidmap)) {
                     // This shouldn't happen...
                     throw new coding_exception("Could not find parent ID $record->parent for format_page.id = $record->id");
                 }
-                $parentid = $oldtonew[$record->parent];
+                $parentid = $pageidmap[$record->parent];
             } else {
                 $parentid = 0;
             }
@@ -179,6 +281,16 @@ function xmldb_format_flexpage_install() {
         $records->close();
 
         // Migrate Page Items
+        $pagemenucmidmap = array();
+        if ($DB->get_manager()->table_exists('pagemenu')) {
+            $pagemenucmidmap = $DB->get_records_sql_menu('
+                SELECT cm.id, cm.instance
+                  FROM {course_modules} cm
+            INNER JOIN {modules} m ON cm.module = m.id
+                 WHERE m.name = ?
+            ', array('pagemenu'));
+        }
+
         $context = false;
         $records = $DB->get_recordset_sql('
             SELECT i.*, c.id AS courseid
@@ -188,7 +300,7 @@ function xmldb_format_flexpage_install() {
           ORDER BY c.id
         ');
         foreach ($records as $record) {
-            if (!array_key_exists($record->pageid, $oldtonew)) {
+            if (!array_key_exists($record->pageid, $pageidmap)) {
                 continue;
             }
             if ($record->position == 'l') {
@@ -202,29 +314,48 @@ function xmldb_format_flexpage_install() {
                 // For blocks, just update the block instance record
                 $DB->update_record('block_instances', (object) array(
                     'id' => $record->blockinstance,
-                    'subpagepattern' => $oldtonew[$record->pageid],
+                    'subpagepattern' => $pageidmap[$record->pageid],
                     'defaultweight' => $record->sortorder,
                     'defaultregion' => $region,
                 ));
             } else {
-                // For modules, insert flexpagemod instance
+                // For modules, insert flexpagemod or flexpagenav instance
                 if (!$context or $context->instanceid != $record->courseid) {
                     $context = get_context_instance(CONTEXT_COURSE, $record->courseid);
                 }
-                $record->blockinstance = $DB->insert_record('block_instances', (object) array(
-                    'blockname' => 'flexpagemod',
-                    'parentcontextid' => $context->id,
-                    'showinsubcontexts' => 0,
-                    'pagetypepattern' => 'course-view-*',
-                    'subpagepattern' => $oldtonew[$record->pageid],
-                    'defaultregion' => $region,
-                    'defaultweight' => $record->sortorder,
-                    'configdata' => base64_encode(serialize((object) array('cmid' => $record->cmid)))
-                ));
-                $DB->insert_record('block_flexpagemod', (object) array(
-                    'instanceid' => $record->blockinstance,
-                    'cmid' => $record->cmid,
-                ));
+                if (array_key_exists($record->cmid, $pagemenucmidmap)) {
+                    $instanceid = $pagemenucmidmap[$record->cmid];
+                    if (array_key_exists($instanceid, $menuidmap)) {
+                        $menuid = $menuidmap[$instanceid];
+                    } else {
+                        $menuid = 0;
+                    }
+                    $record->blockinstance = $DB->insert_record('block_instances', (object) array(
+                        'blockname' => 'flexpagenav',
+                        'parentcontextid' => $context->id,
+                        'showinsubcontexts' => 0,
+                        'pagetypepattern' => 'course-view-*',
+                        'subpagepattern' => $pageidmap[$record->pageid],
+                        'defaultregion' => $region,
+                        'defaultweight' => $record->sortorder,
+                        'configdata' => base64_encode(serialize((object) array('menuid' => $menuid)))
+                    ));
+                } else {
+                    $record->blockinstance = $DB->insert_record('block_instances', (object) array(
+                        'blockname' => 'flexpagemod',
+                        'parentcontextid' => $context->id,
+                        'showinsubcontexts' => 0,
+                        'pagetypepattern' => 'course-view-*',
+                        'subpagepattern' => $pageidmap[$record->pageid],
+                        'defaultregion' => $region,
+                        'defaultweight' => $record->sortorder,
+                        'configdata' => base64_encode(serialize((object) array('cmid' => $record->cmid)))
+                    ));
+                    $DB->insert_record('block_flexpagemod', (object) array(
+                        'instanceid' => $record->blockinstance,
+                        'cmid' => $record->cmid,
+                    ));
+                }
             }
 
             // In order to set visibility, we need to set a block position
@@ -237,7 +368,7 @@ function xmldb_format_flexpage_install() {
                     $bp->blockinstanceid = $instance->id;
                     $bp->contextid = $context->id;
                     $bp->pagetype = 'course-view-flexpage';
-                    $bp->subpage = $oldtonew[$record->pageid];
+                    $bp->subpage = $pageidmap[$record->pageid];
                     $bp->visible = 0;
                     $bp->region = $instance->defaultregion;
                     $bp->weight = $instance->defaultweight;
@@ -247,5 +378,89 @@ function xmldb_format_flexpage_install() {
             }
         }
         $records->close();
+    }
+
+/// Migration of top tabs (we build a new menu that's designated as the top tabs)
+    $records = $DB->get_recordset('course', array('format' => 'flexpage'), '', 'id');
+    foreach ($records as $record) {
+        $links = array();
+
+        $menurecords = $DB->get_recordset('pagemenu', array('course' => $record->id, 'useastab' => 1), 'taborder, name', 'id');
+        foreach ($menurecords as $menurecord) {
+            if (!array_key_exists($menurecord->id, $menuidmap)) {
+                continue;
+            }
+            $link = new block_flexpagenav_model_link();
+            $link->set_type('flexpagenav')
+                 ->set_configs(array(new block_flexpagenav_model_link_config('menuid', $menuidmap[$menurecord->id])));
+            $links[] = $link;
+        }
+        $menurecords->close();
+
+        // (2 | 1) means DISP_THEME and DISP_PUBLISH
+        $pagerecords = $DB->get_recordset_select('format_page', 'courseid = ? AND ((display & ?) = ?) AND parent = 0', array($record->id, (2 | 1), (2 | 1)), 'sortorder');
+        foreach ($pagerecords as $pagerecord) {
+            if (!array_key_exists($pagerecord->id, $pageidmap)) {
+                continue;
+            }
+            $link = new block_flexpagenav_model_link();
+            $link->set_type('flexpagenav')
+                 ->set_configs(array(
+                    new block_flexpagenav_model_link_config('pageid', $pageidmap[$pagerecord->id]),
+                    new block_flexpagenav_model_link_config('children', 0),
+                    new block_flexpagenav_model_link_config('exclude', ''),
+                 )
+            );
+            $links[] = $link;
+        }
+        $pagerecords->close();
+
+        if (!empty($links)) {
+            $menurepo = new block_flexpagenav_repository_menu();
+            $linkrepo = new block_flexpagenav_repository_link();
+
+            $menu = new block_flexpagenav_model_menu();
+            $menu->set_couseid($record->id)
+                 ->set_name(get_string('migrationtoptabs', 'block_flexpagenav'))
+                 ->set_render('navhorizontal')
+                 ->set_displayname(0)
+                 ->set_useastab(1);
+            $menurepo->save_menu($menu);
+
+            $weight = 0;
+            foreach ($links as $link) {
+                $link->set_menuid($menu->get_id())
+                     ->set_weight($weight);
+
+                $linkrepo->save_link($link)
+                         ->save_link_config($link, $link->get_configs());
+
+                $weight++;
+            }
+        }
+    }
+    $records->close();
+
+/// Cleanup block/page_module
+    $instances = $DB->get_records('block_instances', array('blockname' => 'page_module'));
+    if(!empty($instances)) {
+        foreach($instances as $instance) {
+            blocks_delete_instance($instance);
+        }
+    }
+    $DB->delete_records('block', array('name' => 'page_module'));
+    drop_plugin_tables('page_module', "$CFG->dirroot/blocks/page_module/db/install.xml", false);
+    drop_plugin_tables('block_page_module', "$CFG->dirroot/blocks/page_module/db/install.xml", false);
+    capabilities_cleanup('block/page_module');
+    events_uninstall('block/page_module');
+
+/// Cleanup mod/pagemenu
+    uninstall_plugin('mod', 'pagemenu');
+
+/// Cleanup course/format/page
+    foreach (array('format_page', 'format_page_items') as $table) {
+        if ($DB->get_manager()->table_exists($table)) {
+            $DB->get_manager()->drop_table(new xmldb_table($table));
+        }
     }
 }
