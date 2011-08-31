087f933ba23a7ade6c328058c88c165643c6f6f7
Mark Nielsen <mark@moodlerooms.com>
2011-08-31 18:27:48 +0000

    INT-2125: Flexpage help and interfaces should refer to flexpages not pages
    git-svn-id: http://svn.moodlerooms.com/Moodle/2.0/branches/plugins@16551 f529b7b8-409e-e885-f664-9768b4ed42ab

--- lang/en/format_flexpage.php
+++ lang/en/format_flexpage.php
@@ -6,120 +6,120 @@
  * @package format_flexpage
  */
 $string['pluginname'] = 'Flexpage format';
-$string['defaultcoursepagename'] = 'Default Page (Change me)';
-$string['pagenotfound'] = 'The page with id = {$a} does not exist in this course.';
+$string['defaultcoursepagename'] = 'Default flexpage (Change me)';
+$string['pagenotfound'] = 'The flexpage with id = {$a} does not exist in this course.';
 $string['addmenu'] = 'Add';
 $string['managemenu'] = 'Manage';
 $string['addactivityaction'] = 'Add activity';
-$string['addpagesaction'] = 'Add pages';
-$string['managepagesaction'] = 'Manage all pages';
-$string['editpageaction'] = 'Manage page settings';
-$string['editpage'] = 'Page settings';
+$string['addpagesaction'] = 'Add flexpages';
+$string['managepagesaction'] = 'Manage all flexpages';
+$string['editpageaction'] = 'Manage flexpage settings';
+$string['editpage'] = 'Flexpage settings';
 $string['movebefore'] = 'before';
 $string['moveafter'] = 'after';
 $string['movechild'] = 'as first child of';
 $string['ajaxexception'] = 'Application error: {$a}';
-$string['addedpages'] = 'Added pages: {$a}';
-$string['addpages'] = 'Add pages';
+$string['addedpages'] = 'Added flexpages: {$a}';
+$string['addpages'] = 'Add flexpages';
 $string['error'] = 'Error';
 $string['genericasyncfail'] = 'The request failed for an unknown reason, please try the action again.';
 $string['close'] = 'Close';
 $string['gotoa'] = 'Go to "{$a}"';
-$string['movepageaction'] = 'Move page';
-$string['movepage'] = 'Move page';
-$string['movepagea'] = 'Move page <strong>{$a}</strong>';
-$string['movedpage'] = 'Moved page "{$a->movepage}" {$a->move} page "{$a->refpage}"';
+$string['movepageaction'] = 'Move flexpage';
+$string['movepage'] = 'Move flexpage';
+$string['movepagea'] = 'Move flexpage <strong>{$a}</strong>';
+$string['movedpage'] = 'Moved flexpage "{$a->movepage}" {$a->move} flexpage "{$a->refpage}"';
 $string['addactivity'] = 'Add activity';
-$string['addactivity_help'] = 'Choose where you would like to place the new activity on the page by selecting one of the buttons on the top of the <strong>Add activity</strong> window. Next, choose the activity or resource that you would like to add to the course and page.';
+$string['addactivity_help'] = 'Choose where you would like to place the new activity on the flexpage by selecting one of the buttons on the top of the <strong>Add activity</strong> window. Next, choose the activity or resource that you would like to add to the course and flexpage.';
 $string['addto'] = 'Add to:';
 $string['addexistingactivity'] = 'Add existing activity';
-$string['addexistingactivity_help'] = 'Choose where you would like to place the existing activity on the page by selecting one of the buttons on the top of the <strong>Add existing activity</strong> window. Next, place a checkmark next to the activities that you would like to add to this page. Finally, click the "Add activities" button at the bottom of the window to complete the action.';
+$string['addexistingactivity_help'] = 'Choose where you would like to place the existing activity on the flexpage by selecting one of the buttons on the top of the <strong>Add existing activity</strong> window. Next, place a checkmark next to the activities that you would like to add to this flexpage. Finally, click the "Add activities" button at the bottom of the window to complete the action.';
 $string['addexistingactivityaction'] = 'Add existing activity';
 $string['addactivities'] = 'Add activities';
 $string['addblock'] = 'Add block';
-$string['addblock_help'] = 'Choose where you would like to place the block on the page by selecting one of the buttons on the top of the <strong>Add block</strong> window. Next, click on the name of the block that you would like to add to the course.';
+$string['addblock_help'] = 'Choose where you would like to place the block on the flexpage by selecting one of the buttons on the top of the <strong>Add block</strong> window. Next, click on the name of the block that you would like to add to the course.';
 $string['addblockaction'] = 'Add block';
 $string['block'] = 'Block';
 $string['displayhidden'] = 'Hidden';
 $string['displayvisible'] = 'Visible but not in menus';
 $string['displayvisiblemenu'] = 'Visible and in menus';
 $string['navnone'] = 'No navigation';
-$string['navprev'] = 'Previous page only';
-$string['navnext'] = 'Next page only';
-$string['navboth'] = 'Both previous and next page';
+$string['navprev'] = 'Previous flexpage only';
+$string['navnext'] = 'Next flexpage only';
+$string['navboth'] = 'Both previous and next flexpage';
 $string['navigation'] = 'Navigation';
-$string['navigation_help'] = 'Used to display next and/or previous buttons on this page.  The buttons take the user to the next/previous available page.';
+$string['navigation_help'] = 'Used to display next and/or previous buttons on this flexpage.  The buttons take the user to the next/previous available flexpage.';
 $string['display'] = 'Display';
 $string['name'] = 'Name';
-$string['name_help'] = 'This is the name of your page and it will appear to course users in menus and the like.';
-$string['formnamerequired'] = 'The page name is a required field.';
+$string['name_help'] = 'This is the name of your flexpage and it will appear to course users in menus and the like.';
+$string['formnamerequired'] = 'The flexpage name is a required field.';
 $string['regionwidths'] = 'Block region widths';
 $string['regionwidths_help'] = 'One can specify how wide each region of blocks can be in pixels.  An example would be to set left to 200, main to 500 and right to 200.  Please note though that available regions and their names can change from theme to theme.';
-$string['managepages'] = 'Manage pages';
-$string['managepages_help'] = 'From this window, you can view the index of all pages, and quickly manage, move or delete individual pages; as well as alter display settings; as well as control navigation settings.';
-$string['pagename'] = 'Page name';
-$string['deletepage'] = 'Delete page';
-$string['deletepageaction'] = 'Delete page';
-$string['areyousuredeletepage'] = 'Are you sure that you want to permanently delete page <strong>{$a}</strong>?';
-$string['deletedpagex'] = 'Deleted page "{$a}"';
-$string['flexpage:managepages'] = 'Manage pages';
+$string['managepages'] = 'Manage flexpages';
+$string['managepages_help'] = 'From this window, you can view the index of all flexpages, and quickly manage, move or delete an individual flexpage; as well as alter display settings; as well as control navigation settings.';
+$string['pagename'] = 'Flexpage name';
+$string['deletepage'] = 'Delete flexpage';
+$string['deletepageaction'] = 'Delete flexpage';
+$string['areyousuredeletepage'] = 'Are you sure that you want to permanently delete flexpage <strong>{$a}</strong>?';
+$string['deletedpagex'] = 'Deleted flexpage "{$a}"';
+$string['flexpage:managepages'] = 'Manage flexpages';
 $string['pagexnotavailable'] = '{$a} is not available';
 $string['pagenotavailable'] = 'Not available';
-$string['pagenotavailable_help'] = 'This page is not available to you.  Below might be a list of conditions that you must satisfy to in order to view the page.';
+$string['pagenotavailable_help'] = 'This flexpage is not available to you.  Below might be a list of conditions that you must satisfy to in order to view the flexpage.';
 $string['sectionname'] = 'Section';
-$string['page'] = 'Page';
+$string['page'] = 'Flexpage';
 $string['copydotdotdot'] = 'Copy...';
 $string['nextpage'] = 'Next >';
 $string['previouspage'] = '< Previous';
 $string['themelayoutmissing'] = 'Your current theme does not support Flexpage.  Please change the theme (or if enabled, the course theme or your preferred theme in your profile) to one that has a "{$a}" layout.';
-$string['deletemodwarn'] = 'If this activity is deleted, then it will be removed from all pages.';
+$string['deletemodwarn'] = 'If this activity is deleted, then it will be removed from all flexpages.';
 $string['continuedotdotdot'] = 'Continue...';
 $string['warning'] = 'Warning';
 $string['actionbar'] = 'Edit bar';
 $string['availablefrom'] = 'Allow access from';
-$string['availablefrom_help'] = 'This page will be available to course users after this date.';
+$string['availablefrom_help'] = 'This flexpage will be available to course users after this date.';
 $string['availableuntil'] = 'Allow access until';
-$string['availableuntil_help'] = 'This page will be available to course users before this date.';
+$string['availableuntil_help'] = 'This flexpage will be available to course users before this date.';
 $string['showavailability'] = 'Before this can be accessed';
-$string['showavailability_help'] = 'If the page is unavailable to the user, this setting determines if this page\'s restriction information is displayed or nothing at all.';
+$string['showavailability_help'] = 'If the flexpage is unavailable to the user, this setting determines if this flexpage\'s restriction information is displayed or nothing at all.';
 
 
-$string['display_help'] = 'Configure if this page is:
+$string['display_help'] = 'Configure if this flexpage is:
 <ol>
     <li>Hidden completely to non-editors.</li>
     <li>Visible to course users, but does not appear in Flexpage Menus and course navigation.</li>
     <li>Visible to course users and appears in Flexpage Menus and course navigation.</li>
 </ol>';
 
-$string['addpages_help'] = 'From here you can new pages to your course.  Going from left to right on the form:
+$string['addpages_help'] = 'From here you can add new flexpages to your course.  Going from left to right on the form:
  <ol>
-    <li>Enter the name of your page (blank names are not added).</li>
-    <li>The next <em>two</em> drop-downs determine where in the index of pages your new page will be added.  So, you can add your new page before, after or as a child (sub-page) of another page.</li>
-    <li>(Optional) In the last drop-down, one can choose an existing page to copy into your newly created page.</li>
+    <li>Enter the name of your flexpage (blank names are not added).</li>
+    <li>The next <em>two</em> drop-downs determine where in the index of flexpages your new flexpage will be added.  So, you can add your new flexpage before, after or as a child (sub-flexpage) of another flexpage.</li>
+    <li>(Optional) In the last drop-down, one can choose an existing flexpage to copy into your newly created flexpage.</li>
 </ol>
-To add more than one page at a time, click on the "+" icon and fill out the new row.  If you click on the "+" icon too many times, just blank out the page names and they will not be added.';
+To add more than one flexpage at a time, click on the "+" icon and fill out the new row.  If you click on the "+" icon too many times, just blank out the flexpage names and they will not be added.';
 
 $string['actionbar_help'] = '
-<p>With Flexpage, course designers can create multiple pages within the course. Each page can have unique or shared content on them.</p>
+<p>With Flexpage, course designers can create multiple flexpages within the course. Each flexpage can have unique or shared content on them.</p>
 
-<p>With the Edit bar, on can jump to different pages within this course by clicking the name of the page in the drop-down menu.</p>
+<p>With the Edit bar, on can jump to different flexpages within this course by clicking the name of the flexpage in the drop-down menu.</p>
 
 <p>Available actions under the <strong>Add</strong> Edit bar menu item:
     <ul>
-        <li>To add a page, select the <strong>Add pages</strong> link from the drop-down menu. When you add new pages, you will want to determine where they are located in the index of pages. Pages can be children of other pages (sub-pages). Or they can simply be placed before or after other pages. New pages can also be blank, or a copy of an existing page. To add multiple pages to a course, press the "+" icon on the right side of the <strong>Add pages</strong> window.</li>
-        <li>To add a new activity to this page, select <strong>Add activity</strong> link from the drop-down menu. Choose where you would like to place the new activity on the page by selecting one of the buttons on the top of the <strong>Add activity</strong> window. Next, choose the activity or resource that you would like to add to the course and page.</li>
-        <li>To add an existing activity to this page, select <strong>Add existing activity</strong> link from the down-down menu. Choose where you would like to place the existing activity on the page by selecting one of the buttons on the top of the <strong>Add existing activity</strong> window. Next, place a checkmark next to the activities that you would like to add to this page. Finally, click the "Add activities" button at the bottom of the window to complete the action.</li>
-        <li>To add a block to this page, select <strong>Add block</strong> from the drop-down menu. Choose where you would like to place the block on the page by selecting one of the buttons on the top of the <strong>Add block</strong> window. Next, click on the name of the block that you would like to add to the course.</li>
-        <li>To add an existing menu to this page, select <strong>Add existing menu</strong> from the drop-down menu. Choose where you would like to place the block on the page by selecting one of the buttons on the top of the <strong>Add existing menu</strong> window. Next, click on the name of the menu that you would like to add to the course.</li>
+        <li>To add a flexpage, select the <strong>Add flexpages</strong> link from the drop-down menu. When you add new flexpages, you will want to determine where they are located in the index of flexpages. Flexpages can be children of other flexpages (sub-flexpages). Or they can simply be placed before or after other flexpages. New flexpages can also be blank, or a copy of an existing flexpage. To add multiple flexpages to a course, press the "+" icon on the right side of the <strong>Add flexpages</strong> window.</li>
+        <li>To add a new activity to this flexpage, select <strong>Add activity</strong> link from the drop-down menu. Choose where you would like to place the new activity on the flexpage by selecting one of the buttons on the top of the <strong>Add activity</strong> window. Next, choose the activity or resource that you would like to add to the course and flexpage.</li>
+        <li>To add an existing activity to this flexpage, select <strong>Add existing activity</strong> link from the down-down menu. Choose where you would like to place the existing activity on the flexpage by selecting one of the buttons on the top of the <strong>Add existing activity</strong> window. Next, place a checkmark next to the activities that you would like to add to this flexpage. Finally, click the "Add activities" button at the bottom of the window to complete the action.</li>
+        <li>To add a block to this flexpage, select <strong>Add block</strong> from the drop-down menu. Choose where you would like to place the block on the flexpage by selecting one of the buttons on the top of the <strong>Add block</strong> window. Next, click on the name of the block that you would like to add to the course.</li>
+        <li>To add an existing menu to this flexpage, select <strong>Add existing menu</strong> from the drop-down menu. Choose where you would like to place the block on the flexpage by selecting one of the buttons on the top of the <strong>Add existing menu</strong> window. Next, click on the name of the menu that you would like to add to the course.</li>
     </ul>
 </p>
 
 <p>Available actions under the <strong>Manage</strong> Edit bar menu item:
     <ul>
-        <li>To configure the settings for this page, click the <strong>Manage page settings</strong> link from the drop-down menu. From this window, you can edit the page name; change the widths of the page regions; indicate whether the page should be hidden, visible, or visible in menus; as well as determine whether the pages should have "previous and next" navigation buttons.</li>
-        <li>To move a page, click the <strong>Move page</strong> link from the drop-down menu. From this window, you can choose whether the page is a child to another page, or whether it is before or after another page in the index.</li>
-        <li>To delete a page, click the <strong>Delete page</strong> link from the drop-down menu. From this window, you can confirm that want to delete the current page.</li>
-        <li>To manage the settings of multiple pages, click the <strong>Manage all pages</strong> link from the drop-down menu. From this window, you can view the index of all pages, and quickly manage, move or delete individual pages; as well as alter display settings; as well as control navigation settings.</li>
+        <li>To configure the settings for this flexpage, click the <strong>Manage flexpage settings</strong> link from the drop-down menu. From this window, you can edit the flexpage name; change the widths of the flexpage regions; indicate whether the flexpage should be hidden, visible, or visible in menus; as well as determine whether the flexpages should have "previous and next" navigation buttons.</li>
+        <li>To move a flexpage, click the <strong>Move flexpage</strong> link from the drop-down menu. From this window, you can choose whether the flexpage is a child to another flexpage, or whether it is before or after another flexpage in the index.</li>
+        <li>To delete a flexpage, click the <strong>Delete flexpage</strong> link from the drop-down menu. From this window, you can confirm that want to delete the current flexpage.</li>
+        <li>To manage the settings of multiple flexpages, click the <strong>Manage all flexpages</strong> link from the drop-down menu. From this window, you can view the index of all flexpages, and quickly manage, move or delete individual flexpages; as well as alter display settings; as well as control navigation settings.</li>
         <li>To manage Tabs for your course, as well as other menus, click the <strong>Manage all menus</strong> link from the drop-down menu. From this window you can create menus, as well as quickly edit menus, delete menus, and manage links within the menus.</li>
     </ul>
 </p>';
