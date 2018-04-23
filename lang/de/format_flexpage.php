1afaa09a030e0d75efca6080fb20937dd6a25a3f
David Castro <david.castro@blackboard.com>
2018-04-23 15:33:11 +0000

    INT-13192: Importing de lang

---
+++ lang/de/format_flexpage.php
@@ -0,0 +1,186 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Flexpage-Format';
+$string['defaultcoursepagename'] = 'Standard-Flexpage (ändern)';
+$string['pagenotfound'] = 'In diesem Kurs ist keine Flexpage mit der ID = {$a} vorhanden.';
+$string['addmenu'] = 'Hinzufügen';
+$string['managemenu'] = 'Verwalten';
+$string['addactivityaction'] = 'Aktivität hinzufügen';
+$string['addpagesaction'] = 'Flexpages hinzufügen';
+$string['managepagesaction'] = 'Alle Flexpages verwalten';
+$string['editpageaction'] = 'Flexpage-Einstellungen verwalten';
+$string['editpage'] = 'Flexpage-Einstellungen';
+$string['movebefore'] = 'vor';
+$string['moveafter'] = 'nach';
+$string['movechild'] = 'als erste Unterkategorie von';
+$string['ajaxexception'] = 'Anwendungsfehler: {$a}';
+$string['addedpages'] = 'Hinzugefügte Flexpages: {$a}';
+$string['addpages'] = 'Flexpages hinzufügen';
+$string['error'] = 'Fehler';
+$string['genericasyncfail'] = 'Die Anfrage ist aus einem unbekannten Grund fehlgeschlagen. Bitte versuchen Sie die Aktion erneut.';
+$string['close'] = 'Schließen';
+$string['gotoa'] = 'Gehen Sie zu "{$a}"';
+$string['movepageaction'] = 'Flexpage verschieben';
+$string['movepage'] = 'Flexpage verschieben';
+$string['movepagea'] = 'Flexpage verschieben <strong>{$a}</strong>';
+$string['movedpage'] = 'Verschobene Flexpage "{$a->movepage}" {$a->move} Flexpage "{$a->refpage}"';
+$string['addactivity'] = 'Aktivität hinzufügen';
+$string['addactivity_help'] = 'Indem Sie eine der Schaltflächen oben im Fenster <strong>Aktivität hinzufügen</strong> auswählen, können Sie festlegen, wo die neue Aktivität auf der Flexpage platziert werden soll. Wählen Sie als nächstes die Aktivität oder die Arbeitsmaterialien aus, die Sie zu dem Kurs und der Flexpage hinzufügen möchten.';
+$string['addto'] = 'Hinzufügen zum Bereich:';
+$string['addexistingactivity'] = 'Vorhandene Aktivität hinzufügen';
+$string['addexistingactivity_help'] = 'Indem Sie eine der Schaltflächen oben im Fenster <strong>Vorhandene Aktivität hinzufügen</strong> auswählen, können Sie festlegen, wo die vorhandene Aktivität auf der Flexpage platziert werden soll. Setzen Sie nun ein Häkchen neben den Aktivitäten, die Sie zu dieser Flexpage hinzufügen möchten. Klicken Sie zum Schluss unten im Fenster auf die Schaltfläche "Aktivitäten hinzufügen", um die Aktion abzuschließen.';
+$string['addexistingactivityaction'] = 'Vorhandene Aktivität hinzufügen';
+$string['addactivities'] = 'Aktivitäten hinzufügen';
+$string['addblock'] = 'Block hinzufügen';
+$string['addblock_help'] = 'Indem Sie eine der Schaltflächen oben im Fenster <strong>Block hinzufügen</strong> auswählen, können Sie festlegen, wo der Block auf der Flexpage platziert werden soll. Klicken Sie als nächstes auf den Namen des Blocks, den Sie zu dem Kurs hinzufügen möchten.';
+$string['addblockaction'] = 'Block hinzufügen';
+$string['block'] = 'Block';
+$string['displayhidden'] = 'Verborgen';
+$string['displayvisible'] = 'Sichtbar, aber nicht in den Menüs vorhanden';
+$string['displayvisiblemenu'] = 'Sichtbar und in den Menüs vorhanden';
+$string['navnone'] = 'Keine Navigation';
+$string['navprev'] = 'Nur vorige Flexpage';
+$string['navnext'] = 'Nur nächste Flexpage';
+$string['navboth'] = 'Vorige und nächste Flexpage';
+$string['navigation'] = 'Navigation';
+$string['navigation_help'] = 'Wird verwendet, um die Schaltflächen „Weiter“ und/oder „Zurück“ auf dieser Flexpage anzuzeigen.  Über diese Schaltflächen gelangt der Nutzer zu der verfügbaren nächsten/vorigen Flexpage.';
+$string['display'] = 'Anzeigen';
+$string['name'] = 'Name';
+$string['name_help'] = 'Das ist der Name Ihrer Flexpage, der Kursnutzern in Menüs und dergleichen angezeigt wird.';
+$string['formnamerequired'] = 'Der Flexpage-Name ist ein Pflichtfeld.';
+$string['regionwidths'] = 'Blockbereichbreiten';
+$string['regionwidths_help'] = 'Sie können die Breite der Blockbereiche in Pixeln festlegen.  Zum Beispiel kann der linke Bereich auf 200, der Hauptbereich auf 500 und der rechte Bereich auf 200 gesetzt werden.  Bitte beachten Sie aber, dass sich verfügbare Bereiche und ihre Namen von Design zu Design unterscheiden können.';
+$string['managepages'] = 'Flexpages verwalten';
+$string['managepages_help'] = 'In diesem Fenster können Sie den Index aller Flexpages anzeigen, einzelne Flexpages schnell verwalten, verschieben oder löschen, die Anzeigeeinstellung verändern und die Navigationseinstellungen festlegen.';
+$string['pagename'] = 'Flexpage-Name';
+$string['deletepage'] = 'Flexpage löschen';
+$string['deletepageaction'] = 'Flexpage löschen';
+$string['areyousuredeletepage'] = 'Wollen Sie die Flexpage <strong>{$a}</strong> wirklich dauerhaft löschen?';
+$string['deletedpagex'] = 'Gelöschte Flexpage "{$a}"';
+$string['flexpage:managepages'] = 'Flexpages verwalten';
+$string['pagexnotavailable'] = '{$a} ist nicht verfügbar.';
+$string['pagenotavailable'] = 'Nicht verfügbar';
+$string['pagenotavailable_help'] = 'Diese Flexpage ist für Sie nicht verfügbar. Möglicherweise müssen Sie die folgenden Bedingungen erfüllen, damit Sie die Flexpage ansehen können.';
+$string['sectionname'] = 'Abschnitt';
+$string['page'] = 'Flexpage';
+$string['copydotdotdot'] = 'Kopieren...';
+$string['nextpage'] = 'Weiter >';
+$string['next'] = 'Weiter';
+$string['previouspage'] = '< Zurück';
+$string['previous'] = 'Zurück';
+$string['themelayoutmissing'] = 'Ihr aktuelles Design unterstützt keine Flexpage.  Ändern Sie bitte das Design (oder, falls aktiviert, das Kursdesign oder das bevorzugte Design in Ihrem Profil) in eines, das über ein "{$a}"-Layout verfügt.';
+$string['deletemodwarn'] = 'Wird diese Aktivität gelöscht, dann wird sie von allen Flexpages entfernt.';
+$string['continuedotdotdot'] = 'Fortfahren...';
+$string['warning'] = 'Warnung';
+$string['actionbar'] = 'Aktionsleiste';
+$string['availablefrom'] = 'Zugang erlauben ab';
+$string['availablefrom_help'] = 'Diese Flexpage wird den Nutzern des Kurses nach diesem Datum zur Verfügung stehen.';
+$string['availableuntil'] = 'Zugang erlauben bis';
+$string['availableuntil_help'] = 'Diese Flexpage wird den Nutzern des Kurses vor diesem Datum zur Verfügung stehen.';
+$string['showavailability'] = 'Bevor Sie hierauf zugreifen können';
+$string['showavailability_help'] = 'Wenn die Flexpage für den Nutzer nicht verfügbar ist, legt diese Einstellung fest, ob der Hinweis der Flexpage zur Zugangsbeschränkung angezeigt wird oder ob keine Informationen angezeigt werden.';
+$string['nomoveoptionserror'] = 'Diese Flexpage kann nicht verschoben werden, da kein freier Platz zur Verfügung steht.  Versuchen Sie, neue Flexpages vor oder nach dieser Flexpage hinzuzufügen.';
+$string['frontpage'] = 'Flexpage auf der Startseite verwenden';
+$string['frontpagedesc'] = 'Damit wird das Flexpage-Format auf der Startseite aktiviert.';
+$string['hidefromothers'] = 'Flexpage verbergen';
+$string['showfromothers'] = 'Flexpage anzeigen';
+$string['jumptoflexpage'] = 'Zu einer Flexpage springen';
+$string['preventactivityview'] = 'Sie können auf diese Aktivität noch nicht zugreifen, da sie sich auf einer Flexpage befindet, die derzeit nicht für Sie verfügbar ist.';
+$string['showavailability_hide'] = 'Gesamte Flexpage verbergen';
+$string['showavailability_show'] = 'Flexpage grau unterlegen (mit Hinweis zur Zugangsbeschränkung)';
+$string['display_help'] = 'Nehmen Sie Konfigurationseinstellungen für diese Flexpage vor:
+<ol>
+<li>Die Flexpage bleibt für Nicht-Editoren vollständig verborgen.</li>
+<li>Die Flexpage ist für Kursnutzer/innen sichtbar, wird aber in den Flexpage-Menüs und der Kursnavigation nicht angezeigt.</li>
+<li>Die Flexpage ist für Kursnutzer/innen sichtbar und wird in den Flexpage-Menüs und der Kursnavigation angezeigt.</li>
+</ol>';
+$string['addpages_help'] = 'Von hier aus können Sie Ihrem Kurs neue Flexpages hinzufügen.  Auf dem Formular von links nach rechts:
+<ol>
+<li>Geben Sie den Namen Ihrer Flexpage ein (leere Namensfelder werden nicht hinzugefügt).</li>
+<li> Mithilfe der nächsten<em>zwei</em> Dropdown-Menüs können Sie festlegen, an welcher Stelle im Index der Flexpages Ihre neue Flexpage hinzugefügt wird. Sie können Ihre neue Flexpage also vor, nach oder als Unterkategorie (Sub-Flexpage) einer anderen Flexpage hinzufügen.</li>
+<li>(Optional) Im letzten Dropdown-Menü können Sie eine vorhandene Flexpage auswählen, die in Ihre neu erstellte Flexpage kopiert wird.</li>
+</ol>
+Um mehrere Flexpages gleichzeitig hinzuzufügen, klicken Sie auf das "+"-Zeichen und füllen Sie die neue Zeile aus.  Wenn Sie zu häufig auf das "+"-Zeichen geklickt haben, können Sie einfach das Flexpage-Namensfeld leer lassen und die Flexpages werden nicht hinzugefügt.';
+$string['actionbar_help'] = '
+<p>Mit Flexpage können Kurs-Designer mehrere Flexpages innerhalb des Kurses erstellen. Jede Flexpage kann über individuelle oder geteilte Inhalte verfügen.</p>
+
+<p>Mithilfe der Aktionsleiste können Sie innerhalb des Kurses zwischen den verschiedenen Flexpages hin- und herspringen, indem Sie in dem
+ Dropdown-Menü auf den jeweiligen Namen einer Flexpage klicken.</p>
+
+<p>Verfügbare Aktionen unter dem Menüelement <strong>Hinzufügen</strong> der Aktionsleiste:
+    <ul>
+        <li>Um eine Flexpage hinzuzufügen, klicken Sie im Dropdown-Menü auf den Link <strong>Flexpages hinzufügen</strong>. Wenn Sie neue Flexpages hinzufügen,
+        müssen Sie deren Position im Index der Flexpages festlegen. Flexpages können Unterkategorien von anderen Flexpages (Sub-Flexpages) sein. Sie können auch einfach vor oder nach
+        anderen Flexpages platziert werden. Darüber hinaus können Sie leer oder die Kopie einer anderen Flexpage sein. Um mehrere Flexpages zu einem Kurs hinzuzufügen, klicken Sie auf das "+"-Zeichen auf der rechten Seite des Fensters <strong>Flexpages hinzufügen</strong>.</li>
+        <li>Um eine neue Aktivität zu dieser Flexpage hinzuzufügen, wählen Sie im Dropdown-Menü den Link <strong>Aktivität hinzufügen</strong> aus. Indem Sie
+        eine der Schaltflächen oben im Fenster <strong>Aktivität hinzufügen</strong> auswählen, können Sie festlegen, wo die neue Aktivität auf der Flexpage platziert werden soll. Wählen Sie als nächstes die Aktivität oder die Arbeitsmaterialien aus, die Sie zu dem Kurs und der Flexpage hinzufügen möchten.</li>
+        <li>Um eine vorhandene Aktivität zu dieser Flexpage hinzuzufügen, wählen Sie aus dem Dropdown-Menü den Link <strong>Vorhandene Aktivität hinzufügen</strong> aus.
+         Indem Sie eine der Schaltflächen oben im Fenster <strong>Vorhandene Aktivität hinzufügen</strong> auswählen, können Sie festlegen, wo die vorhandene Aktivität auf der
+         Flexpage platziert werden soll. Setzen Sie nun ein Häkchen neben den Aktivitäten, die Sie zu dieser Flexpage hinzufügen möchten. Klicken Sie zum Schluss unten im Fenster auf die Schaltfläche "Aktivitäten hinzufügen", um die Aktion abzuschließen.</li>
+         <li>Um einen Block zu dieser Flexpage hinzuzufügen, wählen Sie aus dem Dropdown-Menü <strong>Block hinzufügen</strong> aus. Indem Sie
+         eine der Schaltflächen oben im Fenster <strong>Block hinzufügen</strong> auswählen, können Sie festlegen, wo der Block auf der Flexpage platziert werden soll. Klicken Sie als Nächstes auf den Namen des Blocks, den Sie zu dem Kurs hinzufügen möchten.</li>
+         <li>Um ein vorhandenes Menü zu dieser Flexpage hinzuzufügen, wählen Sie aus dem Dropdown-Menü <strong>Vorhandenes Menü hinzufügen</strong> aus. Indem Sie
+         eine der Schaltflächen oben im Fenster <strong>Vorhandenes Menü hinzufügen</strong> auswählen, können Sie festlegen, wo der Block auf der Flexpage platziert werden soll. Klicken Sie als nächstes auf den Namen des Menüs, das Sie zu dem Kurs hinzufügen möchten.</li>
+    </ul>
+</p>
+
+<p>Verfügbare Aktionen unter dem Menüelement <strong>Verwalten</strong> der Aktionsleiste:
+    <ul>
+        <li>Um die Einstellungen für diese Flexpage zu konfigurieren, klicken Sie im Dropdown-Menü auf den Link <strong>Flexpage-Einstellungen verwalten</strong>.
+        In diesem Fenster können Sie den Flexpage-Namen bearbeiten, die Breite der Flexpage-Bereiche ändern und angeben, ob die Flexpage verborgen, sichtbar oder in den Menüs sichtbar sein soll. Darüber hinaus können Sie festlegen, ob die Flexpages über die Navigationsschaltflächen "Zurück und Weiter" verfügen sollen.</li>
+        <li>Um eine Flexpage zu verschieben, klicken Sie im Dropdown-Menü auf den Link <strong>Flexpage bewegen</strong>. In diesem Fenster können Sie auswählen, ob die
+         Flexpage eine Unterkategorie einer anderen Flexpage darstellt oder ob sie im Index vor oder nach einer anderen Flexpage angezeigt wird.</li>
+         <li>Um eine Flexpage zu löschen, klicken Sie im Dropdown-Menü auf den Link <strong>Flexpage löschen</strong>. In diesem Fenster können Sie das Löschen der aktuellen Flexpage bestätigen.</li>
+         <li>Um die Einstellungen mehrerer Flexpages zu verwalten, klicken Sie im Dropdown-Menü auf den Link <strong>Alle Flexpages verwalten</strong>. In diesem Fenster können Sie 
+         den Index aller Flexpages anzeigen, einzelne Flexpages schnell verwalten, verschieben oder löschen sowie die Anzeigeeinstellung verändern und die Navigationseinstellungen festlegen.</li>
+         <li>Um Registerkarten und andere Menüs für Ihren Kurs zu verwalten, klicken Sie im Dropdown-Menü auf den Link <strong>Alle Menüs verwalten</strong>. In diesem Fenster
+         können Sie Menüs erstellen, sie schnell bearbeiten oder löschen sowie Links innerhalb der Menüs verwalten.</li>
+    </ul>
+</p>';
+$string['none'] = '(keine)';
+$string['grade_atleast'] = 'mehr als';
+$string['grade_upto'] = 'und weniger als';
+$string['contains'] = 'enthält';
+$string['doesnotcontain'] = 'enthält nicht';
+$string['isempty'] = 'ist leer';
+$string['isequalto'] = 'ist gleich mit';
+$string['isnotempty'] = 'ist nicht leer';
+$string['endswith'] = 'endet mit';
+$string['startswith'] = 'beginnt mit';
+$string['completion_complete'] = 'muss als abgeschlossen markiert sein';
+$string['completion_fail'] = 'muss abgeschlossen sein, aber nicht bestanden';
+$string['completion_incomplete'] = 'darf nicht als abgeschlossen markiert sein';
+$string['completion_pass'] = 'muss abgeschlossen und mit Mindestbewertung zum erfolgreichen Abschluss bestanden sein';
+$string['completioncondition'] = 'Bedingung für Aktivitätsabschluss';
+$string['completioncondition_help'] = 'Diese Einstellung bestimmt, welche Abschlussbedingungen bei Aktivitäten vorliegen müssen, um Zugang zum Bereich zu erhalten. Bitte beachten Sie, dass die Abschlussverfolgung zunächst eingestellt werden muss, bevor die Abschlussbedingungen für Aktivitäten eingestellt werden können.
+
+Auf Wunsch können mehrere Abschlussbedingungen für Aktivitäten gesetzt werden. In diesem Fall wird der Zugang zum Bereich erst gewährt, wenn ALLE Abschlussbedingungen der Aktivitäten erfüllt sind.';
+$string['gradecondition'] = 'Bewertungsbedingung';
+$string['gradecondition_help'] = 'Diese Einstellung legt fest, welche Bewertungen erzielt werden müssen, um auf diesen Kursabschnitt Zugriff zu erhalten.
+
+Wenn mehrere Bewertungen als Voraussetzung definiert werden, müssen ALLE erfüllt sein, bevor ein Zugriff möglich wird.';
+$string['userfield'] = 'Nutzerfeld';
+$string['userfield_help'] = 'Sie können den Zugriff basierend auf jedem beliebigem Feld im Nutzerprofil einschränken.';
+$string['releasecode'] = 'Freigabecode';
+$string['releasecode_help'] = 'Dieses Kurselement steht Teilnehmern erst zur Verfügung, nachdem sie den Freigabecode hier eingegeben haben.';
