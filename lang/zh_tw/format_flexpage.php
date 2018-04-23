7aabd2d12c22d3323cc7eec2ca75b0391823c7ee
David Castro <david.castro@blackboard.com>
2018-04-23 15:43:20 +0000

    INT-13192: Importing zh_tw lang

---
+++ lang/zh_tw/format_flexpage.php
@@ -0,0 +1,176 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Flexpage 格式';
+$string['defaultcoursepagename'] = '預設 Flexpage (變更我)';
+$string['pagenotfound'] = '此課程中不存在編號為 {$a} 的 Flexpage。';
+$string['addmenu'] = '新增';
+$string['managemenu'] = '管理';
+$string['addactivityaction'] = '新增活動';
+$string['addpagesaction'] = '新增 Flexpage';
+$string['managepagesaction'] = '管理所有 Flexpage';
+$string['editpageaction'] = '管理 Flexpage 設定';
+$string['editpage'] = 'Flexpage 設定';
+$string['movebefore'] = '之前';
+$string['moveafter'] = '之後';
+$string['movechild'] = '作為此項目的第一子系：';
+$string['ajaxexception'] = '應用程式錯誤：{$a}';
+$string['addedpages'] = '已新增的 Flexpage：{$a}';
+$string['addpages'] = '新增 Flexpage';
+$string['error'] = '錯誤';
+$string['genericasyncfail'] = '由於未知原因導致請求失敗，請重新嘗試此動作。';
+$string['close'] = '關閉';
+$string['gotoa'] = '移至「{$a}」';
+$string['movepageaction'] = '移動 Flexpage';
+$string['movepage'] = '移動 Flexpage';
+$string['movepagea'] = '移動 Flexpage <strong>{$a}</strong>';
+$string['movedpage'] = '已移動的 Flexpage "{$a->movepage}" {$a->move} Flexpage "{$a->refpage}"';
+$string['addactivity'] = '新增活動';
+$string['addactivity_help'] = '藉由選取「<strong>新增活動</strong>」視窗頂端其中一個按鈕，選擇您要在 Flexpage 上放置新活動的位置。然後，選擇您要新增至課程和 Flexpage 的活動或資源。';
+$string['addto'] = '新增至區域：';
+$string['addexistingactivity'] = '新增現有活動';
+$string['addexistingactivity_help'] = '藉由選取「<strong>新增現有活動</strong>」視窗頂端其中一個按鈕，選擇您要在 Flexpage 上放置現有活動的位置。然後，在您要新增至此 Flexpage 的活動旁放置勾號。最後，按一下視窗底部的「新增活動」按鈕，以完成動作。';
+$string['addexistingactivityaction'] = '新增現有活動';
+$string['addactivities'] = '新增活動';
+$string['addblock'] = '新增區塊';
+$string['addblock_help'] = '藉由選取「<strong>新增區塊</strong>」視窗頂端其中一個按鈕，選擇您要在 Flexpage 上放置區塊的位置。然後，按一下您要新增至課程的區塊名稱。';
+$string['addblockaction'] = '新增區塊';
+$string['block'] = '區塊';
+$string['displayhidden'] = '隱藏';
+$string['displayvisible'] = '可見但不在功能表中';
+$string['displayvisiblemenu'] = '可見且在功能表中';
+$string['navnone'] = '無瀏覽';
+$string['navprev'] = '僅顯示上一頁 Flexpage';
+$string['navnext'] = '僅顯示下一頁 Flexpage';
+$string['navboth'] = '顯示上一頁和下一頁 Flexpage';
+$string['navigation'] = '瀏覽';
+$string['navigation_help'] = '用以在此 Flexpage 上顯示「下一頁」和/或「上一頁」按鈕。按鈕將帶領使用者至下一頁/上一頁可用的 Flexpage。';
+$string['display'] = '顯示';
+$string['name'] = '名稱';
+$string['name_help'] = '這是您的 Flexpage 的名稱，課程使用者會在功能表與其他相似位置中看到此名稱。';
+$string['formnamerequired'] = 'Flexpage 名稱為必填欄位。';
+$string['regionwidths'] = '區塊區域寬度';
+$string['regionwidths_help'] = '您可以指定每個區塊區域寬度的像素，例如可設定左側區塊區域寬 200 像素，主要區塊區域寬 500 像素，右側區塊區域寬 200 像素。惟請注意，可用的區域及其名稱得隨不同主題而有所變更。';
+$string['managepages'] = '管理 Flexpage';
+$string['managepages_help'] = '您可從此視窗檢視所有 Flexpage 的索引，也能在此快速管理、移動或刪除個別 Flexpage、變更顯示設定並控制瀏覽設定。';
+$string['pagename'] = 'Flexpage 名稱';
+$string['deletepage'] = '刪除 Flexpage';
+$string['deletepageaction'] = '刪除 Flexpage';
+$string['areyousuredeletepage'] = '您確定要永久刪除 Flexpage <strong>{$a}</strong> 嗎？';
+$string['deletedpagex'] = '刪除 Flexpage「{$a}」';
+$string['flexpage:managepages'] = '管理 Flexpage';
+$string['pagexnotavailable'] = '無法使用 {$a}';
+$string['pagenotavailable'] = '無法使用';
+$string['pagenotavailable_help'] = '您無法使用此 Flexpage。若要檢視 Flexpage，以下是您必須符合的條件清單。';
+$string['sectionname'] = '章節';
+$string['page'] = 'Flexpage';
+$string['copydotdotdot'] = '複製...';
+$string['nextpage'] = '下一頁 >';
+$string['next'] = '下一頁';
+$string['previouspage'] = '< 上一頁';
+$string['previous'] = '上一頁';
+$string['themelayoutmissing'] = '您目前的主題不支援 Flexpage。請變更為具有「{$a}」版面配置的主題 (或者如果啟用，可變更課程主題或您的資訊中的偏好主題)。';
+$string['deletemodwarn'] = '若刪除此活動，此活動將從所有 Flexpage 中移除。';
+$string['continuedotdotdot'] = '繼續...';
+$string['warning'] = '警告';
+$string['actionbar'] = '動作列';
+$string['availablefrom'] = '允許從此位置存取：';
+$string['availablefrom_help'] = '在此日期之後，課程使用者可使用此 Flexpage。';
+$string['availableuntil'] = '允許存取截止日期：';
+$string['availableuntil_help'] = '在此日期之前，課程使用者可使用此 Flexpage。';
+$string['showavailability'] = '在此日期前可存取';
+$string['showavailability_help'] = '若使用者無法使用 Flexpage，此設定會決定是否顯示此 Flexpage 的限制資訊或完全不顯示。';
+$string['nomoveoptionserror'] = '您無法移動此 Flexpage，因為沒有能放置此 Flexpage 的可用位置。請嘗試在此 Flexpage 之前或之後新增新的 Flexpage。';
+$string['frontpage'] = '在首頁上使用 Flexpage';
+$string['frontpagedesc'] = '這將啟用首頁上的 Flexpage 格式。';
+$string['hidefromothers'] = '隱藏 Flexpage';
+$string['showfromothers'] = '顯示 Flexpage';
+$string['jumptoflexpage'] = '跳至特定 Flexpage';
+$string['preventactivityview'] = '您無法存取此活動，因為此活動位於您目前無法使用的 Flexpage 上。';
+$string['showavailability_hide'] = '完全隱藏 Flexpage';
+$string['showavailability_show'] = '顯示反灰的 Flexpage 及限制資訊';
+$string['display_help'] = '請將此 Flexpage 配置為：
+<ol>
+    <li>對非編輯者完全隱藏。</li>
+    <li>課程使用者可見，但不顯示在 Flexpage 功能表和課程瀏覽中。</li>
+    <li>課程使用者可見，且顯示在 Flexpage 功能表和課程瀏覽中。</li>
+</ol>';
+$string['addpages_help'] = '您可以從這裡將新的 Flexpage 新增至您的課程。表單上從左到右為：
+ <ol>
+    <li>輸入您的 Flexpage 的名稱 (無法新增空白名稱)。</li>
+    <li>下<em>兩</em>個下拉式清單可決定您的新 Flexpage 於 Flexpage 索引內的新增位置。因此，您可以將新的 Flexpage 新增於其他 Flexpage 之前、之後或作為子系 (子 Flexpage)。</li>
+    <li>(可選) 在最後的下拉式清單中，您可以選取某個現有的 Flexpage，將其複製到新建立的 Flexpage 中。</li>
+</ol>
+若要同時新增多個 Flexpage，按一下「+」圖示並填寫新列。若您按了太多次「+」圖示，僅需將 Flexpage 名稱留白，系統便不會新增。';
+$string['actionbar_help'] = '
+<p>使用 Flexpage，課程設計者可以在課程內建立多個 Flexpage。每個 Flexpage 上可以有獨一無二或共用的內容。</p>
+
+<p>使用動作列，您可以在下拉式功能表中按一下 Flexpage 名稱，就能跳到此課程中的不同 Flexpage。</p>
+
+<p>「<strong>新增</strong>」動作列功能表項目之下的可用動作：
+    <ul>
+        <li>若要新增 Flexpage，從下拉式功能表中選取「<strong>新增 Flexpage</strong>」連結。當您新增 Flexpage 時，會想要決定它們在 Flexpage 索引中的位置。Flexpage 可以是其他 Flexpage 的子系 (子 Flexpage)，或者只是置於其他 Flexpage 之前或之後。新的 Flexpage 亦可為空白或是現有 Flexpage 的副本。若要將多個 Flexpage 新增到課程，請按下「<strong>新增 Flexpage</strong>」視窗右側的「+」圖示。</li> 
+        <li>若要將新活動新增到此 Flexpage，請從下拉式功能表中選取「<strong>新增活動</strong>」。藉由選取「 新增活動 」視窗頂端的按鈕之一，選擇您要在 Flexpage 上放置新活動的位置。然後，選擇您要新增至課程和 Flexpage 的活動或資源。藉由選取「<strong>新增活動</strong>」視窗頂端的按鈕之一，選擇您要在 Flexpage 上放置新活動的位置。然後，選擇您要新增至課程和 Flexpage 的活動或資源。</li>
+        <li>若要新增現有活動到此 Flexpage，請從下拉式功能表中選取「<strong>新增現有活動</strong>」。
+        藉由選取「<strong>新增現有活動</strong>」視窗頂端的按鈕之一，選擇您要在 Flexpage 上放置現有活動的位置。然後，在您要新增至此 Flexpage 的活動旁放置勾號。最後，按一下視窗底部的「新增活動」按鈕，以完成動作。</li>
+        <li>若要新增區塊到此 Flexpage，請從下拉式功能表中選取「<strong>新增區塊</strong>」。藉由選取「<strong>新增區塊</strong>」視窗頂端的按鈕之一，選擇您要在 Flexpage 上放置區塊的位置。然後，按一下您要新增至課程的區塊名稱。</li>
+        <li>若要新增現有功能表到此 Flexpage，請從下拉式功能表中選取「<strong>新增現有功能表</strong>」。藉由選取「<strong>新增現有功能表</strong>」視窗頂端的按鈕之一，選擇您要在 Flexpage 上放置區塊的位置。然後，按一下您要新增至課程的功能表名稱。</li>
+    </ul>
+</p>
+
+<p>「<strong>管理</strong>」動作列功能表項目之下的可用動作：
+    <ul>
+        <li>若要配置此 Flexpage 的設定，請從下拉式功能表中按一下「<strong>管理 Flexpage 設定</strong>」連結。您可以從此視窗編輯 Flexpage 名稱，變更 Flexpage 區域寬度，指定 Flexpage 為隱藏、可見或在功能表中可見，以及決定 Flexpage 是否應有「上一頁」和「下一頁」為瀏覽按鈕。</li>
+        <li>若要移動 Flexpage，請從下拉式功能表中按一下「<strong>移動 Flexpage</strong>」連結。您可以從此視窗選擇 Flexpage 是否為另一 Flexpage 的子系，或者在索引中是否置於另一 Flexpage 之前或之後。</li>
+        <li>若要刪除 Flexpage，請從下拉式功能表中按一下「<strong>刪除 Flexpage</strong>」連結。您可以從此視窗確認您想刪除目前的 Flexpage。</li>
+        <li>若要管理多個 Flexpage 的設定，請從下拉式功能表中按一下「<strong>管理所有 Flexpage</strong>」連結。您可從此視窗檢視所有 Flexpage 的索引，也能在此快速管理、移動或刪除個別 Flexpage、變更顯示設定並控制瀏覽設定。</li>
+        <li>若要管理您的課程頁籤和其他功能表，請從下拉式功能表中按一下「<strong>管理所有功能表</strong>」連結。您可以從此視窗建立功能表、快速編輯功能表、刪除功能表，以及管理功能表內的連結。</li>
+    </ul>
+</p>';
+$string['none'] = '(無)';
+$string['grade_atleast'] = '必須至少為';
+$string['grade_upto'] = '且小於';
+$string['contains'] = '包含';
+$string['doesnotcontain'] = '不包含';
+$string['isempty'] = '空白';
+$string['isequalto'] = '等於';
+$string['isnotempty'] = '非空白';
+$string['endswith'] = '結尾是';
+$string['startswith'] = '開頭是';
+$string['completion_complete'] = '必須標記為「完成」';
+$string['completion_fail'] = '成績必須為不及格才算完成';
+$string['completion_incomplete'] = '不可標記為「完成」';
+$string['completion_pass'] = '成績必須為及格才算完成';
+$string['completioncondition'] = '活動完成條件';
+$string['completioncondition_help'] = '此設定會決定若要存取 Flexpage 所必須符合的任何活動完成條件。請注意：在設定活動完成條件之前，必須先設定完成追蹤。
+
+如有需要，可以設定多項活動完成條件。若是如此，唯有所有活動完成條件皆符合時才允許存取 Flexpage。';
+$string['gradecondition'] = '成績條件';
+$string['gradecondition_help'] = '此設定會決定若要存取 Flexpage 所必須符合的成績條件。
+
+如有需要，可以設定多項成績條件。若是如此，唯有所有成績條件皆符合時，才允許存
+取 Flexpage。';
+$string['userfield'] = '使用者欄位';
+$string['userfield_help'] = '您可以根據使用者資訊的任何欄位限制存取。';
+$string['releasecode'] = '發佈碼';
+$string['releasecode_help'] = '學員在取得發佈碼且輸入於此之前，無法使用此課程項目。';
